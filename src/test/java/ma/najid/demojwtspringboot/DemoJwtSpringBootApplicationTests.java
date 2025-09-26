ebab2177467eed918240d4f0ecc249f599942e1b
NAJID-MOHAMED <najidmohamed2026@gmail.com>
2025-09-26 13:28:36 +0100

    first commit jwt

---
+++ src/test/java/ma/najid/demojwtspringboot/DemoJwtSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.najid.demojwtspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
